8c96e35551d841c116c4f98ba7bd0908f773d208
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-14 15:12:26 +0000

    finished variable exercise

--- ruby_variables_starter_code/person.rb
+++ ruby_variables_starter_code/person.rb
@@ -1,24 +1,33 @@
-class Person 
+class Person
 
     def name
         # create a name variable
         name = "Steve"
-    end 
+    end
 
     def age
         # create age variable here
-
+        age = 18
     end
 
     def children
         # create children array here
-
+        children = ['john', 'jack', 'jill', 'james']
 
     end
 
     def address
-
         # create address hash here
+        address = {
+          street: 'street',
+          road: 'road',
+          house_number: 5,
+          town: 'town',
+          county: 'county',
+          postcode: 'postcode',
+          email_addresses: ['email@email.com', 'email2']
+
+        }
 
     end
 
@@ -31,7 +40,7 @@ class Person
           mothers_name: "Eve",
           birth_town: "Richmond"
         }
-
+      password = favourite_things[1]+ ' '+ memorable_stuff[:birth_town]
     end
 
 end
